40074e7f3244453dbc619b0c3fc4a8f79de2238f
SDD PC <SDD PC@DESKTOP-TB0671S>
2025-10-13 16:17:53 +0600

    last changed

--- WebAPI/obj/Debug/net9.0/WebAPI.AssemblyInfo.cs
+++ WebAPI/obj/Debug/net9.0/WebAPI.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("WebAPI")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a3721eaaa1cc619c84c4d19b9c13c331be92004b")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0191e8c8d4b8eda1dcd90141207fcdab7e79d15a")]
 [assembly: System.Reflection.AssemblyProductAttribute("WebAPI")]
 [assembly: System.Reflection.AssemblyTitleAttribute("WebAPI")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
